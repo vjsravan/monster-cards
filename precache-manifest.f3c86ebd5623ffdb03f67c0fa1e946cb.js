23ae2596e655f926a0c729e8eb781272f2de37b1
vjsravan <60521157+vjsravan@users.noreply.github.com>
2020-06-15 16:13:11 -0400

    Updates

---
+++ precache-manifest.f3c86ebd5623ffdb03f67c0fa1e946cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b6e193176fad9a3b94d",
+    "url": "/monsters-card/static/css/main.d140282b.chunk.css"
+  },
+  {
+    "revision": "4b6e193176fad9a3b94d",
+    "url": "/monsters-card/static/js/main.17575d8f.chunk.js"
+  },
+  {
+    "revision": "83bb6a5dd83bf3917fb5",
+    "url": "/monsters-card/static/js/runtime~main.06a124a7.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monsters-card/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "87ae4f0f36a2713154822aa9ca5cb82f",
+    "url": "/monsters-card/index.html"
+  }
+];
